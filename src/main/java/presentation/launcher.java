68dcec200f01d7eab9bb6259829b8baf7dbe6898
pilselias@gmail.com <b6qoB9j6FSXS>
2021-12-16 14:50:21 +0100

    JavaFX POM.xml

---
+++ src/main/java/presentation/launcher.java
@@ -0,0 +1,10 @@
+package presentation;
+
+import javafx.application.Application;
+import presentation.app.app;
+
+public class launcher {
+    public static void main(String[] args) {
+        Application.launch(app.class, args);
+    }
+}

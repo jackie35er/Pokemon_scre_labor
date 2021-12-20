5d66a386a293c80e9f71a8bec750a4da998857bb
Susuwatari <b6qoB9j6FSXS>
2021-12-20 14:39:30 +0100

    Background is loaded, waiting for repository

--- src/main/java/presentation/app/app.java
+++ src/main/java/presentation/app/app.java
@@ -6,11 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class app extends Application{
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("src/main/resources/PokemonFXML.fxml"));
+        URL url = new File("src/main/resources/PokemonFXML.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
+
         stage.setScene(scene);
         stage.setTitle("Pokemon");
         stage.show();
--- src/main/java/presentation/controller/controller.java
+++ src/main/java/presentation/controller/controller.java
@@ -1,13 +1,58 @@
 package presentation.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class controller implements Initializable{
+    @FXML
+    private ImageView allyGender;
+
+    @FXML
+    private Rectangle allyHealthbar;
+
+    @FXML
+    private ImageView allyImg;
+
+    @FXML
+    private Label allyLvl;
+
+    @FXML
+    private Label allyName;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private Label emenyName;
+
+    @FXML
+    private ImageView enemyGender;
+
+    @FXML
+    private Rectangle enemyHealthbar;
+
+    @FXML
+    private ImageView enemyImg;
+
+    @FXML
+    private Label enemyLvl;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
+        loadImages();
+    }
 
+    private void loadImages(){
+        background.setImage(new Image("file:src/main/resources/background.png"));
     }
 }

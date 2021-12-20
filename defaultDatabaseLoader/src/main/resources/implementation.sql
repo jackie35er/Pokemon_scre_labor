5a037687d1afd2ee044c7fe629adfbcfb3f305f2
jackie35er <jackie35er@gmail.com>
2021-12-20 15:29:58 +0100

    Added Types and typ multipliers to PokemonInserter and Database

--- defaultDatabaseLoader/src/main/resources/implementation.sql
+++ defaultDatabaseLoader/src/main/resources/implementation.sql
@@ -1,14 +1,32 @@
 use [db_pokemon_goetz_pils]
 
-drop table if exists Pokemons;
 
-create table Pokemons(
+
+drop table if exists Multipliers;
+drop table if exists Pokemons;
+drop table if exists Types;
+
+create table Types(
     id int primary key,
     name varchar(255),
-    hp int,
-    attack int,
-    defense int,
-    sp_attack int,
-    sp_defense int,
-    speed int,
+)
+
+create table Pokemons(
+     id int primary key,
+     name varchar(255),
+     hp int,
+     attack int,
+     defense int,
+     sp_attack int,
+     sp_defense int,
+     speed int,
+     primary_type int foreign key references Types(id),
+     secondary_type int foreign key references Types(id)
+)
+
+create table Multipliers(
+    userType int foreign key references Types(id),
+    targetType int foreign key references Types(id),
+    multiplier double precision,
+    primary key (userType,targetType)
 )

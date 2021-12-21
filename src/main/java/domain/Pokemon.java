e4e2760cb1fbde1299728f9b34eddea28bbb955d
Susuwatari <b6qoB9j6FSXS>
2021-12-21 09:09:15 +0100

    Repository

--- src/main/java/domain/Pokemon.java
+++ src/main/java/domain/Pokemon.java
@@ -1,27 +1,7 @@
 package domain;
 
-public class Pokemon {
-    private final Integer id;
-    private String name;
-    private Integer hp;
-    private Integer attack;
-    private Integer defense;
-    private Integer spAttack;
-    private Integer spDefense;
-    private Integer speed;
-    private Type primaryType;
-    private Type secondaryType;
-
-    public Pokemon(Integer id, String name, Integer hp, Integer attack, Integer defense, Integer spAttack, Integer spDefense, Integer speed, Type primaryType, Type secondaryType) {
-        this.id = id;
-        this.name = name;
-        this.hp = hp;
-        this.attack = attack;
-        this.defense = defense;
-        this.spAttack = spAttack;
-        this.spDefense = spDefense;
-        this.speed = speed;
-        this.primaryType = primaryType;
-        this.secondaryType = secondaryType;
-    }
+public record Pokemon (Integer id,
+                       String name,
+                        Stats stats,
+                        Types types){
 }
---
+++ src/main/java/domain/Types.java
@@ -0,0 +1,5 @@
+package domain;
+
+public record Types (Type primaryType,
+                     Type secondaryType){
+}
